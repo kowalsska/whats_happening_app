7e57ec81127fa1314c84a8fa4d86b42057b1997d
kowalsska <kowalsska2@gmail.com>
2019-03-09 21:58:22 +0100

    Adding Redux

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import mainReducer from './reducers/MainReducer';
 import Geocoder from 'react-native-geocoding';
 import GOOGLE_API from './assets/data/credentials'
+
+const store = createStore(friendReducer);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,7 +17,10 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <AppNavigator />
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+
     );
   }
 }
